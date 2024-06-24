23e9247557c8a181c7c8579c1414f86b26e14780
suptronics <39259807+suptronics@users.noreply.github.com>
2024-06-24 08:40:24 +0800

    Add files via upload

---
+++ x873-bookworm.sh
@@ -0,0 +1,72 @@
+#IMPORTANT! This script is only for the x873 /X878
+#x873 x878 Powering on /reboot /full shutdown through hardware
+#!/bin/bash
+
+    sudo sed -e '/shutdown/ s/^#*/#/' -i /etc/rc.local
+
+    echo '#!/bin/bash
+
+SHUTDOWN=516
+REBOOTPULSEMINIMUM=200
+REBOOTPULSEMAXIMUM=600
+echo "$SHUTDOWN" > /sys/class/gpio/export
+echo "in" > /sys/class/gpio/gpio$SHUTDOWN/direction
+BOOT=529
+echo "$BOOT" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio$BOOT/direction
+echo "1" > /sys/class/gpio/gpio$BOOT/value
+
+echo "X873 Shutting down..."
+
+while [ 1 ]; do
+  shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+  if [ $shutdownSignal = 0 ]; then
+    /bin/sleep 0.2
+  else  
+    pulseStart=$(date +%s%N | cut -b1-13)
+    while [ $shutdownSignal = 1 ]; do
+      /bin/sleep 0.02
+      if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMAXIMUM ]; then
+        echo "X873 Shutting down", SHUTDOWN, ", halting Rpi ..."
+        sudo poweroff
+        exit
+      fi
+      shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
+    done
+    if [ $(($(date +%s%N | cut -b1-13)-$pulseStart)) -gt $REBOOTPULSEMINIMUM ]; then 
+      echo "X873 Rebooting", SHUTDOWN, ", recycling Rpi ..."
+      sudo reboot
+      exit
+    fi
+  fi
+done' > /etc/x873pwr.sh
+sudo chmod +x /etc/x873pwr.sh
+sudo sed -i '$ i /etc/x873pwr.sh &' /etc/rc.local 
+
+
+#X873 full shutdown through Software
+#!/bin/bash
+
+    sudo sed -e '/button/ s/^#*/#/' -i /etc/rc.local
+
+    echo '#!/bin/bash
+
+BUTTON=535
+
+echo "$BUTTON" > /sys/class/gpio/export;
+echo "out" > /sys/class/gpio/gpio$BUTTON/direction
+echo "1" > /sys/class/gpio/gpio$BUTTON/value
+
+SLEEP=${1:-4}
+
+re='^[0-9\.]+$'
+if ! [[ $SLEEP =~ $re ]] ; then
+   echo "error: sleep time not a number" >&2; exit 1
+fi
+
+echo "X873 Shutting down..."
+/bin/sleep $SLEEP
+
+echo "0" > /sys/class/gpio/gpio$BUTTON/value
+' > /usr/local/bin/x873softsd.sh
+sudo chmod +x /usr/local/bin/x873softsd.sh
